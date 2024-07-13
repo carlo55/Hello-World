58a782e4b17b7566a075127c10e2b515d4460d1e
carlo55 <carlo.bianchi@c55.it>
2024-07-13 09:41:20 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    // URL del Google Apps Script
+    const url = 'https://script.google.com/macros/s/AKfycby9wCzyyBJzc_NZ-cV8Qsa8_QVIGSu6Edulrf4IydC7OraFWproBIlKngpHoqSb8lVn/exec';
+
+    // Eseguire una richiesta AJAX
+    fetch(url)
+        .then(response => response.json()) // Convertire la risposta in JSON
+        .then(data => {
+            // Visualizzare il risultato nella pagina
+            document.getElementById('result').innerText = JSON.stringify(data);
+        })
+        .catch(error => {
+            console.error('Errore:', error);
+        });
+});
